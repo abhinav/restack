875383953c32674d6b2f36d52690b6a912750d16
Abhinav Gupta <mail@abhinavg.net>
2017-10-14 14:39:06 -0700

    Add --version flag
    This adds a --version flag to the restack command.

--- cmd/restack/main.go
+++ cmd/restack/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/abhinav/restack"
 	flags "github.com/jessevdk/go-flags"
 )
 
@@ -14,10 +17,26 @@ func main() {
 }
 
 func run() error {
-	// No global options at this time.
-	var opts struct{}
+	var opts struct {
+		Version bool `long:"version" description:"Prints the current version of restack."`
+	}
 
 	parser := flags.NewParser(&opts, flags.HelpFlag)
+
+	// If --version was specified, a command probably was not. So we need to
+	// make subcommands optional and validate manually.
+	parser.SubcommandsOptional = true
+	parser.CommandHandler = func(c flags.Commander, args []string) error {
+		switch {
+		case opts.Version:
+			fmt.Printf("restack v%s\n", restack.Version)
+		case c == nil:
+			parser.WriteHelp(os.Stderr)
+		default:
+			return c.Execute(args)
+		}
+		return nil
+	}
 
 	if _, err := parser.AddCommand(
 		"setup", "Sets up restack",
@@ -28,7 +47,7 @@ func run() error {
 	}
 
 	if _, err := parser.AddCommand(
-		"edit", "Edits a git-rebase-todo",
+		"edit", "Edits the instruction list for an interactive rebase.",
 		"Edits a git-rebase-todo with branch restacking. "+
 			"This is typically called directly by git after a `restack setup`.",
 		newEditCmd(),
---
+++ version.go
@@ -0,0 +1,4 @@
+package restack
+
+// Version is the version of restack currently being used.
+const Version = "0.1.0"
