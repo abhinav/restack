5a3678316144dd3aaf51731a56027d051e6df9e3
Abhinav Gupta <mail@abhinavg.net>
2022-08-27 10:37:13 -0700

    Delete Go code

--- version.go
+++
@@ -1,8 +0,0 @@
-package restack
-
-// Version is the version of restack currently being used.
-//
-// Note that semantic versioning in this repository applies to the `restack`
-// executable, not to its library components. These APIs may break at any
-// time without warning.
-const Version = "0.5.4"
