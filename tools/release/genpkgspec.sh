49e977778aa3486a2c1d068b85810f6ca6364438
Abhinav Gupta <mail@abhinavg.net>
2022-11-06 12:52:27 -0800

    ci/release: Publish Homebrew tap and AUR -bin package
    Add scripts to generate a binary-only Homebrew formula,
    and a binary AUR package from a template,
    and publish them to my Homebrew tap and the AUR repository
    automatically on release.

---
+++ tools/release/genpkgspec.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# genpkgspec.sh VERSION TARBALLS
+#
+# Run this with paths to tarballs named restack-$os-$arch.tar.gz.
+# This will generate target/formula/ and target/aur-bin.
+
+DIR=$(dirname "$0")
+BOTTLE_TMPL="$DIR/bottle.tmpl"
+PKGBUILD_TMPL="$DIR/PKGBUILD.tmpl"
+SRCINFO_TMPL="$DIR/SRCINFO.tmpl"
+
+err() {
+	echo >&2 "$@"
+}
+
+if [[ $# -lt 2 ]]; then
+	err "USAGE: $0 VERSION FILES ..."
+	exit 1
+fi
+
+export VERSION="$1"; shift
+while [[ $# -gt 0 ]]; do
+	FILE="$1"; shift
+	SHA=$(sha256sum "$FILE" | awk '{print $1}')
+	# This is absolutely horrendous,
+	# but it'll work with all versions of Bash.
+	eval "$(perl -se '
+		$file =~ /(darwin|linux)[-_](amd64|arm64|armv7)/
+			or die "Could not match: $file";
+		print "export SHASUM_$1_$2=$shasum\n"
+	' -- -file="$FILE" -shasum="$SHA")"
+done
+
+VARS=(
+	VERSION
+	SHASUM_darwin_amd64
+	SHASUM_darwin_arm64
+	SHASUM_linux_amd64
+	SHASUM_linux_arm64
+	SHASUM_linux_armv7
+)
+shellformat=""
+for VAR in "${VARS[@]}"; do
+	if [[ -z "${!VAR:-}" ]]; then
+		err "Unset variable $VAR"
+		exit 1
+	fi
+	shellformat="$shellformat \$$VAR"
+done
+
+mkdir -p target/formula
+envsubst "$shellformat" < "$BOTTLE_TMPL" > target/formula/restack.rb
+
+mkdir -p target/aur-bin
+envsubst "$shellformat" < "$PKGBUILD_TMPL" > target/aur-bin/PKGBUILD
+envsubst "$shellformat" < "$SRCINFO_TMPL" > target/aur-bin/.SRCINFO
